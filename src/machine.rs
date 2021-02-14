dbc6a22fcd2761b5f04548899d496e231f74588b
Boyd Johnson <johnson.boyd@gmail.com>
2021-02-14 03:19:11 +0000

    Add recurrence check

--- src/machine.rs
+++ src/machine.rs
@@ -1,15 +1,19 @@
-use std::io::Write;
-
 use crate::{
     program::Program,
     tape::Tape,
     types::{State, Symbol},
 };
+use itertools::Either;
+use std::{collections::BTreeMap, io::Write};
+
+type Action<S, Sym> = (S, Sym);
+type Beeps<S> = BTreeMap<S, usize>;
+type Snapshots<S, Sym> = BTreeMap<Action<S, Sym>, Vec<(usize, usize, i64, Tape<Sym>, Beeps<S>)>>;
 
 pub struct Machine<State, Symbol> {
     prog: Program<State, Symbol>,
     state: State,
-    pos: i64,
+    pos: usize,
     tape: Tape<Symbol>,
 
     halt: Option<Halt>,
@@ -36,10 +40,7 @@ impl<S: State, Sym: Symbol> Machine<S, Sym> {
     }
 
     pub fn marks(&self) -> usize {
-        self.tape
-            .iter()
-            .filter(|item| Sym::zero() != **item)
-            .count()
+        self.tape.iter().filter(|item| Sym::zero() != *item).count()
     }
 
     fn write(&mut self, symbol: Sym) {
@@ -60,18 +61,187 @@ impl<S: State, Sym: Symbol> Machine<S, Sym> {
 
     fn input_to_tape(&mut self, input: Vec<Sym>) {
         for (i, s) in input.into_iter().enumerate() {
-            self.tape.write(i as i64, s);
+            self.tape.write(i, s);
         }
     }
 
-    fn write_to_buffer<B: Write>(output: &mut Option<B>, step: usize, new_state: S, symbol: Sym) {
-        if let Some(buffer) = output {
-            writeln!(
-                buffer,
-                "step: {}: state={:?}, symbol: {:?}",
-                step, new_state, symbol
-            )
-            .expect("Failed to write to stdout");
+    fn recurr_check_init() -> (Snapshots<S, Sym>, Vec<i64>) {
+        (BTreeMap::new(), vec![])
+    }
+
+    fn recurr_deviations(&self, deviations: Option<&mut Vec<i64>>, init: usize) -> i64 {
+        if let Some(dev) = deviations {
+            dev.push(self.pos as i64 - init as i64);
+        }
+        self.pos as i64 - init as i64
+    }
+
+    fn recurr_check(
+        &mut self,
+        step: usize,
+        snapshots: Option<&mut Snapshots<S, Sym>>,
+        deviations: Option<&Vec<i64>>,
+        check: Option<usize>,
+        init: usize,
+        beeps: &Beeps<S>,
+        dev: i64,
+    ) -> Option<Halt> {
+        if let (Some(snaps), Some(deviations), Some(recur)) = (snapshots, deviations, check) {
+            let action = (self.state, self.read().copied().unwrap_or_else(Sym::zero));
+            if step >= recur {
+                let mut iter = if let Some(items) = snaps.get(&action) {
+                    Either::Right(items.iter())
+                } else {
+                    Either::Left(std::iter::empty())
+                };
+
+                if let Some((pstep, step, pbeeps, ptape)) = loop {
+                    if let Some((pstep, pinit, pdev, ptape, pbeeps)) = iter.next() {
+                        let (prev, curr) = if dev < *pdev {
+                            let dmax =
+                                deviations[*pstep..].iter().max().copied().unwrap_or(dev) + 1;
+
+                            let prev = ptape
+                                .iter_to((*pinit as i64 + dmax) as usize)
+                                .collect::<Vec<_>>();
+
+                            let mut curr = self
+                                .tape
+                                .iter_to((init as i64 + dmax + dev - *pdev) as usize)
+                                .collect::<Vec<_>>();
+
+                            let mut first = vec![];
+                            for i in 0..prev.len() {
+                                if curr.get(i).is_none() {
+                                    first.push(Sym::zero());
+                                }
+                            }
+
+                            first.append(&mut curr);
+                            (prev, first)
+                        } else if (*pdev) < dev {
+                            let dmin = deviations[*pstep..].iter().min().copied().unwrap_or(dev);
+
+                            let from_prev = if *pinit as i64 + dmin < 0 {
+                                0
+                            } else {
+                                *pinit as i64 + dmin
+                            };
+
+                            let prev = ptape.iter_from(from_prev as usize).collect::<Vec<_>>();
+
+                            let from_curr = if init as i64 + dmin + dev - pdev < 0 {
+                                0
+                            } else {
+                                init as i64 + dmin + dev - pdev
+                            };
+
+                            let mut curr =
+                                self.tape.iter_from(from_curr as usize).collect::<Vec<_>>();
+
+                            for i in 0..prev.len() {
+                                if curr.get(i).is_none() {
+                                    curr.push(Sym::zero());
+                                }
+                            }
+
+                            (prev, curr)
+                        } else {
+                            let dmax =
+                                deviations[*pstep..].iter().max().copied().unwrap_or(dev) + 1;
+                            let dmin = deviations[*pstep..].iter().min().copied().unwrap_or(dev);
+
+                            let from_prev = if *pinit as i64 + dmin < 0 {
+                                0
+                            } else {
+                                *pinit as i64 + dmin
+                            };
+
+                            let prev = ptape
+                                .iter_between(from_prev as usize, (*pinit as i64 + dmax) as usize)
+                                .collect::<Vec<_>>();
+
+                            let from_curr = if init as i64 + dmin < 0 {
+                                0
+                            } else {
+                                init as i64 + dmin
+                            };
+
+                            let curr = self
+                                .tape
+                                .iter_between(from_curr as usize, (init as i64 + dmax) as usize)
+                                .collect::<Vec<_>>();
+
+                            (prev, curr)
+                        };
+
+                        if prev == curr {
+                            break Some((*pstep, step, pbeeps, ptape));
+                        }
+                    } else {
+                        break None;
+                    }
+                } {
+                    self.tape = ptape.clone();
+                    if pbeeps
+                        .keys()
+                        .all(|state| beeps.get(state) > pbeeps.get(state))
+                    {
+                        return Some(Halt::new(pstep, HaltReason::Recurr(step - pstep)));
+                    } else {
+                        return Some(Halt::new(pstep, HaltReason::Quasihalt(step - pstep)));
+                    }
+                }
+
+                snaps
+                    .entry(action)
+                    .and_modify(|v| v.push((step, init, dev, self.tape.clone(), beeps.clone())))
+                    .or_insert_with(|| vec![(step, init, dev, self.tape.clone(), beeps.clone())]);
+            }
+        }
+        None
+    }
+
+    fn write_tape<B: Write>(&self, output: &mut Option<B>, step: usize) {
+        if let Some(b) = output {
+            let mut buffer = format!("{:8} {:?}  ", step, self.state);
+
+            let tape_iter = self.tape.iter();
+
+            for (idx, s) in tape_iter.enumerate() {
+                if idx == self.pos {
+                    buffer.push_str("[");
+                }
+                buffer.push_str(&s.to_string());
+                if idx == self.pos {
+                    buffer.push_str("]")
+                }
+            }
+
+            writeln!(b, "{}", buffer).unwrap();
+        }
+    }
+
+    fn run_turing_step(&mut self, init: &mut usize) {
+        let symbol = self.read().copied().unwrap_or_else(Sym::zero);
+        let state = self.state;
+
+        let &(new_state, symbol, direction) = self.prog.instruction(state, symbol);
+
+        self.state = new_state;
+
+        self.write(symbol);
+
+        match direction {
+            crate::types::Direction::Left => {
+                if self.pos == 0 {
+                    *init += 1;
+                    self.tape.insert();
+                } else {
+                    self.move_left();
+                }
+            }
+            crate::types::Direction::Right => self.move_right(),
         }
     }
 
@@ -83,29 +253,43 @@ impl<S: State, Sym: Symbol> Machine<S, Sym> {
         check_recurrence: Option<usize>,
     ) {
         self.input_to_tape(input);
+        let mut init = self.tape.size() / 2;
 
-        Self::write_to_buffer(output, 0, self.state, Sym::zero());
+        self.pos = init;
 
-        for step in 1..=limit {
-            let symbol = self.read().copied().unwrap_or_else(Sym::zero);
-            let state = self.state;
+        let mut beeps: Beeps<S> = BTreeMap::new();
 
-            let &(new_state, symbol, direction) = self.prog.instruction(state, symbol);
+        let (mut snapshots, mut deviations) = if check_recurrence.is_some() {
+            let f = Self::recurr_check_init();
+            (Some(f.0), Some(f.1))
+        } else {
+            (None, None)
+        };
 
-            Self::write_to_buffer(output, step, new_state, symbol);
+        for step in 0..=limit {
+            self.write_tape(output, step);
 
-            self.state = new_state;
+            let dev = self.recurr_deviations(deviations.as_mut(), init);
 
-            self.write(symbol);
-
-            match direction {
-                crate::types::Direction::Left => self.move_left(),
-                crate::types::Direction::Right => self.move_right(),
+            self.halt = self.recurr_check(
+                step,
+                snapshots.as_mut(),
+                deviations.as_ref(),
+                check_recurrence,
+                init,
+                &beeps,
+                dev,
+            );
+            if self.halt.is_some() {
+                break;
             }
+
+            beeps.insert(self.state, step);
+            self.run_turing_step(&mut init);
 
             // Checks for stopping
             if self.state == S::halt() {
-                self.halt = Some(Halt::new(step, HaltReason::Halt));
+                self.halt = Some(Halt::new(step + 1, HaltReason::Halt));
                 break;
             }
         }
@@ -132,7 +316,10 @@ impl Halt {
     }
 
     pub fn is_lr_recurrence(&self) -> bool {
-        self.reason == HaltReason::Recurr
+        match self.reason {
+            HaltReason::Recurr(_) => true,
+            _ => false,
+        }
     }
 
     pub fn is_limit(&self) -> bool {
@@ -143,8 +330,9 @@ impl Halt {
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum HaltReason {
     Halt,
-    Recurr,
+    Recurr(usize),
     XLimit,
+    Quasihalt(usize),
 }
 
 pub fn run_machine<S: State, Sym: Symbol>(
