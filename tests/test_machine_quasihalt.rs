1b6a48b494838fca6403a2f46bbb637cfcb659bd
Boyd Johnson <johnson.boyd@gmail.com>
2021-10-28 14:59:16 -0500

    Update tests for new spacing scheme

--- tests/test_machine_quasihalt.rs
+++ tests/test_machine_quasihalt.rs
@@ -6,50 +6,50 @@ use lin_rado_turing::{
 
 const QUASIHALTING: &[(&str, usize, usize, usize, &str)] = &[
     // 2/2 (not better than BB)
-    ("1RB 1LB 1LB 1LA", 3, 6, 1, "2-2"),
-    ("1RB 1LB 0LB 1LA", 2, 6, 1, "2-2"),
-    ("1RB 0LB 1LB 1LA", 2, 6, 1, "2-2"),
-    ("1RB 0LB 0LB 1LA", 1, 6, 1, "2-2"),
+    ("1RB 1LB  1LB 1LA", 3, 6, 1, "2-2"),
+    ("1RB 1LB  0LB 1LA", 2, 6, 1, "2-2"),
+    ("1RB 0LB  1LB 1LA", 2, 6, 1, "2-2"),
+    ("1RB 0LB  0LB 1LA", 1, 6, 1, "2-2"),
     // 2/3
-    ("1RB 2LB 1RA 2LB 2LA 0RA", 10, 43, 1, "2-3"), // BBB sigma
-    ("1RB 2LB 1LA 2LB 2RA 0RA", 8, 59, 1, "2-3"),  // BBB shift
-    ("1RB 0LB 1RA 1LB 2LA 2RA", 3, 45, 1, "2-3"),
-    ("1RB 2RA 2LB 2LB 2LA 0LA", 5, 40, 1, "2-3"),
-    ("1RB 1LA 2RA 2LA 2LB 2RB", 8, 17, 2, "2-3"),
+    ("1RB 2LB 1RA  2LB 2LA 0RA", 10, 43, 1, "2-3"), // BBB sigma
+    ("1RB 2LB 1LA  2LB 2RA 0RA", 8, 59, 1, "2-3"),  // BBB shift
+    ("1RB 0LB 1RA  1LB 2LA 2RA", 3, 45, 1, "2-3"),
+    ("1RB 2RA 2LB  2LB 2LA 0LA", 5, 40, 1, "2-3"),
+    ("1RB 1LA 2RA  2LA 2LB 2RB", 8, 17, 2, "2-3"),
     // 3/2
-    ("1RB 0LB 1LA 0RC 1LC 1LA", 6, 55, 1, "3-2"), // BBB shift
-    ("1RB 1RC 1LC 1RA 1RA 1LA", 6, 9, 2, "3-2"),  // BBB sigma
-    ("1RB 0LB 1RC 0RC 1LC 1LA", 6, 54, 1, "3-2"),
-    ("1RB 0LC 1LB 0RC 1LC 1LA", 5, 52, 1, "3-2"),
-    ("1RB 0LC 0LC 0RC 1LC 1LA", 5, 51, 1, "3-2"),
-    ("1RB 0LC 1LA 0RC 1RC 1RB", 5, 49, 1, "3-2"),
-    ("1RB 0LC 0RC 0RC 1LC 1LA", 5, 48, 1, "3-2"),
-    ("1RB 1RC 1LC 0LB 1RA 1LA", 5, 22, 2, "3-2"),
+    ("1RB 0LB  1LA 0RC  1LC 1LA", 6, 55, 1, "3-2"), // BBB shift
+    ("1RB 1RC  1LC 1RA  1RA 1LA", 6, 9, 2, "3-2"),  // BBB sigma
+    ("1RB 0LB  1RC 0RC  1LC 1LA", 6, 54, 1, "3-2"),
+    ("1RB 0LC  1LB 0RC  1LC 1LA", 5, 52, 1, "3-2"),
+    ("1RB 0LC  0LC 0RC  1LC 1LA", 5, 51, 1, "3-2"),
+    ("1RB 0LC  1LA 0RC  1RC 1RB", 5, 49, 1, "3-2"),
+    ("1RB 0LC  0RC 0RC  1LC 1LA", 5, 48, 1, "3-2"),
+    ("1RB 1RC  1LC 0LB  1RA 1LA", 5, 22, 2, "3-2"),
     // 4/2
-    ("1RB 0LC 1LD 0LA 1RC 1RD 1LA 0LD", 0, 66349, 1, "4-2"), // BBB shift
-    ("1RB 1RC 1LC 1RD 1RA 1LD 0RD 0LB", 69, 2819, 1, "4-2"), // BBB sigma
-    ("1RB 1RA 0RC 0RB 0RD 1RA 1LD 1LB", 0, 2568, 1, "4-2"),
-    ("1RB 1RA 0RC 1LA 1LC 1LD 0RB 0RD", 0, 2512, 1, "4-2"),
-    ("1RB 1RC 1RD 0LC 1LD 0LD 1LB 0RA", 56, 2332, 3, "4-2"),
-    ("1RB 0LC 1RC 1LD 1RD 0RB 0LB 1LA", 35, 1460, 3, "4-2"), // QH 1459
-    ("1RB 0LD 1LC 0RD 0LC 1LA 1RA 0RB", 25, 1459, 1, "4-2"),
-    ("1RB 1LC 1LC 0RD 1LA 0LB 1LD 0RA", 39, 1164, 1, "4-2"),
-    ("1RB 1LB 1RC 0LD 0RD 0RA 1LD 0LA", 20, 1153, 1, "4-2"),
+    ("1RB 0LC  1LD 0LA  1RC 1RD  1LA 0LD", 0, 66349, 1, "4-2"), // BBB shift
+    ("1RB 1RC  1LC 1RD  1RA 1LD  0RD 0LB", 69, 2819, 1, "4-2"), // BBB sigma
+    ("1RB 1RA  0RC 0RB  0RD 1RA  1LD 1LB", 0, 2568, 1, "4-2"),
+    ("1RB 1RA  0RC 1LA  1LC 1LD  0RB 0RD", 0, 2512, 1, "4-2"),
+    ("1RB 1RC  1RD 0LC  1LD 0LD  1LB 0RA", 56, 2332, 3, "4-2"),
+    ("1RB 0LC  1RC 1LD  1RD 0RB  0LB 1LA", 35, 1460, 3, "4-2"), // QH 1459
+    ("1RB 0LD  1LC 0RD  0LC 1LA  1RA 0RB", 25, 1459, 1, "4-2"),
+    ("1RB 1LC  1LC 0RD  1LA 0LB  1LD 0RA", 39, 1164, 1, "4-2"),
+    ("1RB 1LB  1RC 0LD  0RD 0RA  1LD 0LA", 20, 1153, 1, "4-2"),
     // 5/2
     (
-        "1RB 1LC 1LC 1RA 1LB 0LD 1LA 0RE 1RD 1RE",
+        "1RB 1LC  1LC 1RA  1LB 0LD  1LA 0RE  1RD 1RE",
         504,
         221032,
         2,
         "5-2",
     ),
     // 2/4
-    ("1RB 2RB 1LB 1LA 1LB 3RA 3LA 2RB", 3340, 2333909, 1, "2-4"),
-    ("1RB 2RB 3LA 2RA 1LB 1LA 1LB 3RB", 63, 22465, 1, "2-4"), // QH 22402  Commentted out are off by 1, mine one less
-    ("1RB 2LA 1RA 1LA 2LB 3LA 2RB 2RA", 107, 10459, 3, "2-4"), // QH 10353
-    ("1RB 2LA 1RA 1LA 3LA 1LB 2RB 2RA", 90, 7193, 2, "2-4"),  // QH 7106
-    ("1RB 2LA 1RA 1LA 3LA 1LB 2RB 2LA", 84, 6443, 2, "2-4"),  // QH 6362
-    ("1RB 2RB 1LA 1LA 2LB 2RA 3LB 1LA", 63, 4068, 1, "2-4"),  // QH 4005
+    ("1RB 2RB 1LB 1LA  1LB 3RA 3LA 2RB", 3340, 2333909, 1, "2-4"),
+    ("1RB 2RB 3LA 2RA  1LB 1LA 1LB 3RB", 63, 22465, 1, "2-4"), // QH 22402  Commentted out are off by 1, mine one less
+    ("1RB 2LA 1RA 1LA  2LB 3LA 2RB 2RA", 107, 10459, 3, "2-4"), // QH 10353
+    ("1RB 2LA 1RA 1LA  3LA 1LB 2RB 2RA", 90, 7193, 2, "2-4"),  // QH 7106
+    ("1RB 2LA 1RA 1LA  3LA 1LB 2RB 2LA", 84, 6443, 2, "2-4"),  // QH 6362
+    ("1RB 2RB 1LA 1LA  2LB 2RA 3LB 1LA", 63, 4068, 1, "2-4"),  // QH 4005
 ];
 
 #[test]
@@ -105,10 +105,10 @@ fn parse_program_and_assert(
     marks: usize,
     steps: usize,
     period: usize,
-    complexity: &str,
+    _complexity: &str,
     parallel: bool,
 ) {
-    let program = parse_program(prog_str, complexity).unwrap();
+    let program = parse_program(prog_str).unwrap();
 
     match program {
         ProgramT::TwoTwo(prog) => assert_machine(prog, marks, steps, period, parallel),
